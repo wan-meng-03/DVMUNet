e2c4e2c86e3d3b0e88d4d7453ac3e8872679ac7e
Dream <19888482552@163.com>
2026-09-05 18:44:16 +0800

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,8 +1,1 @@
-"""Public placeholder for the unreleased DVMUNet implementation."""
 
-
-def build_model(*args, **kwargs):
-    raise NotImplementedError(
-        "The complete DVMUNet model implementation will be released upon "
-        "acceptance of the paper."
-    )
